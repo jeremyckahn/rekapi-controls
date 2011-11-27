e76474f0f16cefab766e069ea8defffecc065abd
Jeremy Kahn <jeremyckahn@gmail.com>
2011-11-27 15:37:52 -0600

    Added build-deps.js.

---
+++ build-deps.sh
@@ -0,0 +1,6 @@
+cat \
+  lib/jquery-ui/jquery.ui.core.js \
+  lib/jquery-ui/jquery.ui.widget.js \
+  lib/jquery-ui/jquery.ui.mouse.js \
+  lib/jquery-ui/jquery.ui.slider.js \
+  > lib/rekapi-controls.jquery-ui.js
