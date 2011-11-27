c2cbe1ed833e45f2b35112ac4c92c11da16875de
Jeremy Kahn <jeremyckahn@gmail.com>
2011-11-27 15:42:11 -0600

    Added Rekapi deps.

--- build-deps.sh
+++ build-deps.sh
@@ -4,3 +4,9 @@ cat \
   lib/jquery-ui/jquery.ui.mouse.js \
   lib/jquery-ui/jquery.ui.slider.js \
   > lib/rekapi-controls.jquery-ui.js
+
+cat \
+  lib/rekapi/lib/underscore/underscore-min.js \
+  lib/rekapi/lib/shifty/builds/shifty.min.js \
+  lib/rekapi/dist/rekapi.min.js \
+  > lib/rekapi-controls.rekapi-bundle.js
