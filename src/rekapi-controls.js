16b1e953f83f71ddc0836ddcb622b6bb07f665cd
Jeremy Kahn <jeremyckahn@gmail.com>
2011-11-28 00:48:30 -0600

    Made the controls nicers.

--- src/rekapi-controls.js
+++ src/rekapi-controls.js
@@ -42,28 +42,69 @@
     self = this;
     $canvas = $(this.canvas);
     $controls = $(Mustache.to_html(CONTROL_TEMPLATE));
+    $canvas.after($controls);
+
+    // Update the reference to the element in the DOM
+    $controls = $canvas.next();
+    this._$controls = $controls;
     $play =     $controls.find('.rekapi-controls-play');
     $pause =    $controls.find('.rekapi-controls-pause');
     $stop =     $controls.find('.rekapi-controls-stop');
     $timeline = $controls.find('.rekapi-controls-timeline');
     $timeline.slider();
-    $canvas.after($controls);
     $controls.width(this.canvas_width());
+    this.controlsUpdatePlayState();
     $timeline.width(computeTimelineWidth(this, $controls));
 
     $play.on('click', function (evt) {
       evt.preventDefault();
-      self.play();
+      self
+        .play()
+        .controlsUpdatePlayState();
     });
 
     $pause.on('click', function (evt) {
       evt.preventDefault();
-      self.pause();
+      self
+        .pause()
+        .controlsUpdatePlayState();
     });
 
     $stop.on('click', function (evt) {
-      self.stop(true);
+      evt.preventDefault();
+      self
+        .stop(true)
+        .controlsUpdatePlayState();
     });
+
+    return this;
+  };
+
+
+  gk.prototype.controlsUpdatePlayState = function () {
+    var $play
+        ,$pause
+
+    $play = this._$controls.find('.rekapi-controls-play');
+    $pause = this._$controls.find('.rekapi-controls-pause');
+
+    if (this.isPlaying()) {
+      $play.css({
+        'display': 'none'
+      });
+
+      $pause.css({
+        'display': 'block'
+      });
+    } else {
+      $play.css({
+        'display': 'block'
+      });
+
+      $pause.css({
+        'display': 'none'
+      });
+    }
 
     return this;
   };
