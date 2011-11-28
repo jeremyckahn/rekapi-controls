30c0522e6faadfa7e9218bfb04e6f4439f60d964
Jeremy Kahn <jeremyckahn@gmail.com>
2011-11-28 00:24:02 -0600

    Bunch of style fixes.

--- src/rekapi-controls.js
+++ src/rekapi-controls.js
@@ -2,11 +2,19 @@
   var gk,
       $
       CONTROL_TEMPLATE = [
-        '<div class="rekapi-controls">'
-          ,'<button class="rekapi-controls-play">Play</button>'
-          ,'<button class="rekapi-controls-pause">Pause</button>'
-          ,'<div class="rekapi-controls-timeline"></div>'
-          ,'<button class="rekapi-controls-stop">Stop</button>'
+        '<div class="rekapi-controls-wrapper">'
+          ,'<div class="rekapi-controls">'
+            ,'<a class="rekapi-controls-button rekapi-controls-play ui-corner-all" href="#">'
+              ,'<span class="ui-icon ui-icon-play"></span>'
+            ,'</a>'
+            ,'<a class="rekapi-controls-button rekapi-controls-pause ui-corner-all" href="#">'
+              ,'<span class="ui-icon ui-icon-pause"></span>'
+            ,'</a>'
+            ,'<div class="rekapi-controls-timeline"></div>'
+            ,'<a class="rekapi-controls-button rekapi-controls-stop ui-corner-all" href="#">'
+              ,'<span class="ui-icon ui-icon-stop"></span>'
+            ,'</a>'
+          ,'</div>'
         ,'</div>'
       ].join('');
 
@@ -17,10 +25,15 @@
   gk = global.Kapi;
   $ = jQuery;
 
+
+  function computeTimelineWidth (kapi, $controls) {
+    return kapi.canvas_width() - $controls.find('.rekapi-controls').width();
+  }
+
   gk.prototype.controlsCreate = function () {
     var self
-        ,$controls
         ,$canvas
+        ,$controls
         ,$play
         ,$pause
         ,$stop
@@ -35,12 +48,16 @@
     $timeline = $controls.find('.rekapi-controls-timeline');
     $timeline.slider();
     $canvas.after($controls);
+    $controls.width(this.canvas_width());
+    $timeline.width(computeTimelineWidth(this, $controls));
 
     $play.on('click', function (evt) {
+      evt.preventDefault();
       self.play();
     });
 
     $pause.on('click', function (evt) {
+      evt.preventDefault();
       self.pause();
     });
 
