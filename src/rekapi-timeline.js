40d0c36d29277c96e98d0bea004d90798f15cbdd
Jeremy Kahn <jeremyckahn@gmail.com>
2012-01-18 21:49:28 -0800

    Added some files for Timeline.

--- src/rekapi-timeline.js
+++ src/rekapi-timeline.js
@@ -3,6 +3,17 @@
     throw 'Rekapi, and jQuery are needed for Rekapi Controls.';
   }
 
+  var CONTAINER_TEMPLATE = [
+    '<div class="rekapi-timeline-wrapper">'
+      ,'<div class="rekapi-timeline-control-bar">'
+      ,'</div>'
+      ,'<div class="rekapi-timeline-actor-headers">'
+      ,'</div>'
+      ,'<div class="rekapi-timeline-actor-timelines">'
+      ,'</div>'
+    ,'</div>'
+  ];
+
   var $ = jQuery;
 
   function RekapiTimeline (kapi) {
