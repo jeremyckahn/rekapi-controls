0f530016fde4e4b38910a521ab1c674112165743
Jeremy Kahn <jeremyckahn@gmail.com>
2012-01-24 22:16:42 -0800

    Actor heeaders print.

--- src/rekapi-timeline.js
+++ src/rekapi-timeline.js
@@ -16,18 +16,41 @@
 
   var $ = jQuery;
 
+
+  function fillControls (controls) {
+    fillHeader(controls);
+  }
+
+
+  function fillHeader (controls) {
+    var actors = controls.kapi.getAllActors();
+    _.each(actors, function (actor, actorId) {
+      var actorHeader = $(document.createElement('li'), {
+        'id': actorId
+      });
+
+      actorHeader.html('Actor ' + actor.id);
+      actorHeader.appendTo(controls.headers)
+    });
+  }
+
+
   function RekapiTimeline (kapi) {
     this.kapi = kapi;
-    this.wrapper = $(CONTAINER_TEMPLATE);
-    this.wrapper.appendTo(document.body);
-    this.render();
+    var wrapper = $(CONTAINER_TEMPLATE);
+    wrapper.appendTo(document.body);
+    this.wrapper = $(document.body).children().last();
+    this.controlBar = this.wrapper.find('.rekapi-timeline-control-bar');
+    this.headers = this.wrapper.find('.rekapi-timeline-actor-headers');
+    this.timeline = this.wrapper.find('.rekapi-timeline-actor-timelines');
+    this.renderControl();
 
     return this;
   }
 
 
-  RekapiTimeline.prototype.render = function () {
-
+  RekapiTimeline.prototype.renderControl = function () {
+    fillControls(this);
   };
 
   global.RekapiTimeline = RekapiTimeline;
