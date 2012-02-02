1514a6bab6be9725b0d3700149c732dca89889f3
Jeremy Kahn <jeremyckahn@gmail.com>
2012-02-01 21:30:51 -0800

    Getting closer to a usable data model.

--- src/rekapi-timeline.js
+++ src/rekapi-timeline.js
@@ -34,8 +34,13 @@
           ,'<div class="' + CSS_NS + 'control-bar">'
           ,'</div>'
           ,'<div class="' + CSS_NS + 'headers-and-timelines">'
-            ,'<ul class="' + CSS_NS + 'actor-headers"></ul>'
-            ,'<ul class="' + CSS_NS + 'actor-timelines"></ul>'
+            ,'<ul class="' + CSS_NS + 'actor-headers">'
+              // Temporary el, used to size the header correctly when it is
+              // initialized
+              ,'<li class="' + CSS_NS + 'actor-header"></li>'
+            ,'</ul>'
+            ,'<ul class="' + CSS_NS + 'actor-timelines">'
+            ,'</ul>'
           ,'</div'
         ,'</div>'
       ].join('')
@@ -67,21 +72,13 @@
     }
 
 
-    //,'__fillHeader': function () {
-      //var actors = this.model.attributes.source.getAllActors();
-      //_.each(actors, function (actor, actorId) {
-        //new RekapiActorHeaderView({
-          //'model': new RekapiActorModel(actor)
-          //,'$parentList': this.$headers
-        //});
-      //}, this);
-    //}
-
-
     ,'renderHeader': function () {
+      this.$headers.children().remove();
       this.model.get('actors').each(function (actor) {
-
-      });
+        console.log(actor)
+        actor.headerView.render();
+        this.$headers.append(actor.headerView.$el);
+      }, this);
 
     }
 
@@ -131,20 +128,14 @@
 
 
     ,'initialize': function (opts) {
-      //this.$parentList = opts.$parentList;
-      //this.id = this.model.get('id');
-
       this.$el = $(this.ACTOR_HEADER_TEMPLATE);
-      //$el.appendTo(this.$parentList);
-      //this.$el = this.$parentList.children(':last');
-      //this.render();
 
       return this;
     }
 
 
     ,'render': function () {
-      this.$el.html('Actor ' + this.id);
+      this.$el.html('Actor ' + this.model.get('source').id);
     }
 
   });
