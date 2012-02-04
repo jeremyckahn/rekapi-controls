8ff2ba0a499d7271f026efe1fc28bedd4d521411
Jeremy Kahn <jeremyckahn@gmail.com>
2012-02-04 11:13:12 -0800

    Stubbed out the Tickmark view.

---
+++ src/rekapi-timeline/view/view.rekapi-tickmarks.js
@@ -0,0 +1,11 @@
+extend('RekapiTimeline.view.rekapiTickmarks', Backbone.View.extend({
+
+  'TEMPLATE': [
+      '<div class="' + RekapiTimeline.constant.CSS_NS + 'tickmarks"'></div>
+    ].join('')
+
+  ,'initialize': function (opts) {
+
+  }
+
+});
