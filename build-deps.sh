32163685b13f274f25ad08e84667e25a2184408d
Jeremy Kahn <jeremyckahn@gmail.com>
2013-03-05 21:22:40 -0800

    Removes Rekapi sobmodules and switches to a static copy.

--- build-deps.sh
+++ build-deps.sh
@@ -1,13 +1,13 @@
 cat \
-  lib/dragon/src/*.js \
+  components/jquery-dragon/src/*.js \
   > dist/dragon-bundle.js
 
 echo "Built Dragon dependency bundle.  Gzipped size:"
 echo `cat dist/dragon-bundle.js | gzip -9f | wc -c` "bytes"
 
-cp lib/rekapi/dist/rekapi.bundle.min.js dist/
+cp lib/rekapi-underscore-shifty.min.js dist/
 echo "Rekapi bundle size, gzipped:"
-echo `cat dist/rekapi.bundle.min.js | gzip -9f | wc -c` "bytes"
+echo `cat lib/rekapi-underscore-shifty.min.js | gzip -9f | wc -c` "bytes"
 
-cp lib/dragon/src/css/jquery.dragon-slider.css dist/
+cp components/jquery-dragon/src/css/jquery.dragon-slider.css dist/
 cp -r src/css/images/ dist/images/
